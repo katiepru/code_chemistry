45809ddf5f0e9b08de7f78d73dfff1a561cd7f38
Josh Matthews <josh@jmatthews.us>
2013-03-16 13:18:17 -0400

    Initial commit. Adding rails default setup.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeChemistry::Application.config.secret_token = '97564e1b4bd1a6dda2b091095045940f66b3dbc87bf561c9e6400a3b5a2cdf8a4f0ca5aa59103b65a533ef7ac5b727ebb1afdbccd70fae4725b15e613a96750e'
