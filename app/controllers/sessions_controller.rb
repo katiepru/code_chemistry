3ebf1d63a8399fbe3955e0b27017201075b427d5
katiepru <katiepru@nbcs.rutgers.edu>
2013-03-16 19:10:08 -0400

    omniauth working with user creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,17 @@ class SessionsController < ApplicationController
   def new
   end
 
-#  def create
-#    auth_hash = request.env['omniauth.auth']
-#    @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-#    if @authorization
-#      render :text => "Welcome back #{@authorization.user.username}! You have already signed up."
-#    else
-#      user = User.new :name => auth_hash["user"]["username"], :email => auth_hash["user"]["email"]
-#      user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
-#      user.save
-#      render :text => "Hi #{user.username}! You've signed up."
-#    end
-#  end
+  def create
+    auth_hash = request.env['omniauth.auth']
+    @authorization = User.find_by_username(auth_hash["info"]["nickname"])
+    if @authorization
+      render :text => "Welcome back #{@authorization.username}! You have already signed up."
+    else
+      user = User.new :username => auth_hash["info"]["nickname"], :email => auth_hash["info"]["email"], :auth_token => auth_hash["credentials"]["token"]
+      user.save
+      render :text => "Hi #{user.username}! You've signed up."
+    end
+  end
 
   def failure
     render :text => "Sorry, you didn't allow access to our app!"
@@ -24,9 +23,9 @@ class SessionsController < ApplicationController
     render :text => "You've logged out!"
   end
 
-  def create
-   auth_hash = request.env['omniauth.auth']
-   render :text => "<pre>" + auth_hash.to_yaml + "</pre>"
-  end
+#  def create
+#   auth_hash = request.env['omniauth.auth']
+#   render :text => "<pre>" + auth_hash.to_yaml + "</pre>"
+#  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :auth_token, :email, :username
-  has_one :authorizations
   validates :username, :email, :presence => true
 end
