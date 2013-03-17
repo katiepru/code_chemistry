84cfc910bd3d456f59a71014120887a637d410f5
Josh Matthews <josh@jmatthews.us>
2013-03-17 09:45:52 -0400

    still broken

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -6,14 +6,15 @@ module ProjectHelper
       if hash and hash['path']
         unless hash['path'].downcase == "readme" || hash['path'].downcase == "readme.md"
           parse_file = open("https://raw.github.com/"+owner+"/"+repo+"/master/"+hash['path'])
-		  prev_line = nil
+          prev_line = nil
           parse_file.each_line do |line|
             curr_line = line
-            if not prev_line
+            if prev_line.nil?
               prev_line = curr_line
             else
               if curr_line[/\A */].size > prev_line[/\A */].size
                 tabs_used = (curr_line[/\A */].size - prev_line[/\A */].size).to_s
+                return tabs_used
               end
             end
           end
