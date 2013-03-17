365bb30dd4b8aebcc5f8f9658fcd15fcc4e81993
Josh Matthews <josh@jmatthews.us>
2013-03-17 01:05:02 -0400

    Editing indents now works.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130317040953) do
+ActiveRecord::Schema.define(:version => 20130317041723) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"
@@ -27,8 +27,7 @@ ActiveRecord::Schema.define(:version => 20130317040953) do
     t.string   "auth_token"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.integer  "spaces"
-    t.integer  "tabs"
+    t.integer  "indent"
   end
 
 end
