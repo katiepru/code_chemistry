9a162afa09580f8dc01f6eb236cfcda036213b01
katiepru <katiepru@nbcs.rutgers.edu>
2013-03-17 10:48:02 -0400

    lang breakdown started

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,14 @@
 module UserHelper
+
+  def calc_ratio(langs_hash, lang)
+    total_lines = 0
+	  lang_lines = 0
+	  langs_hash.each do |l|
+	    total += l[1].to_i
+	    if l[0] == lang
+         lang_lines = l[1].to_i
+	    end
+	  end
+		return(((lang_lines*100)/total_lines).to_s)+"%"
+  end
 end
