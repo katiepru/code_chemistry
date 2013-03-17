5e8f09986065ffb167252ae916184d71863b11ef
Josh Matthews <josh@jmatthews.us>
2013-03-16 21:08:20 -0400

    Adding tab and space attributes to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Main identifiers
   attr_accessible :auth_token, :email, :username
+
+  # Coding style attributes
+  attr_accessible :tabs, :spaces
   validates :username, :email, :presence => true
 end
