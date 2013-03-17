c3291a37c24ecb7a432f2a96cd4dc2e7130a628f
katiepru <katiepru@nbcs.rutgers.edu>
2013-03-16 22:07:34 -0400

    adding some basic methods to user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,4 +4,25 @@ class UserController < ApplicationController
 
   def profile
   end
+
+  def list
+  	@users = User.find(:all)
+  end
+
+  def show
+    @user = User.find_by_username(params[:username])
+  end
+
+  def edit
+    @user = User.find_by_username(params[:username])
+  end
+
+  def update
+    @user = User.find_by_username(params[:username])
+	if @user.update_attributes(params[:user])
+      redirect_to :action => 'show', :is => @user
+	else
+	  render :action => 'edit'
+	end
+  end
 end
